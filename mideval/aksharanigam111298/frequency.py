8efe911b6772f0ec9e772e0d72dbf1f164893a95
ayushnagar123 <an431999@gmail.com>
2019-10-04 04:30:55 +0530

    mid eval final commit

---
+++ mideval/aksharanigam111298/frequency.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+def training(x,y,days):
+    avg = []
+    for i in range(0,np.size(x)-1):
+
+        if(abs(x[i+1][0]-x[i][0]) == days):
+            avg.append(y[i+1])
+
+    return np.mean(avg)
+
+def testing(x,y,trained,days):
+    err = []
+    for i in range(0,np.size(x)-1):
+        if(abs(x[i+1][0]-x[i][0])==days):
+            err.append((abs(y[i+1] - trained)))
+
+    return np.mean(err)
+
+if __name__ == "__main__":
+    df = pd.read_csv('smart_list.csv')
+    print(df.shape)
+    array = df.values
+
+    x = array[:,0:1]
+    y = array[:,1]
+
+    # Splitting the data t 67% training and 33% testing
+
+    xtrain , xtest , ytrain , ytest = train_test_split(x , y , test_size=0.33 , shuffle=False )
+
+    total_err = 0
+    total_trained = 0
+    days = 21
+
+    if(days<=15):
+        trained = training(xtrain , ytrain , days)
+
+        err = testing(xtest , ytest , trained , days)
+
+        print("%.3f , %.3f "%(trained , err))
+
+    else:
+        ctr= 0
+        while(days>15):
+            trained = training(xtrain , ytrain , 15)
+
+            total_trained+=trained
+
+            err = testing(xtest , ytest , trained , 15)
+
+            total_err+=err
+            ctr+=1
+            days-=15
+
+        trained = training(xtrain, ytrain, days)
+        err = testing(xtest, ytest, trained, days)
+
+        total_trained+=trained
+        total_err+=err
+        ctr+=1
+
+        print("%.3f , %.3f"%(total_trained , total_err/ctr))
+
---
+++ mideval/server/client/build/precache-manifest.6ff99f7118df9cbd3f8ff94a70412acb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "018ec3665236e8b2426547603f3c0b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a83724f5a49a16dff54",
+    "url": "/static/css/main.4ed9bdda.chunk.css"
+  },
+  {
+    "revision": "d11fc4876ce6d3177f81",
+    "url": "/static/js/2.2886ec69.chunk.js"
+  },
+  {
+    "revision": "9a83724f5a49a16dff54",
+    "url": "/static/js/main.445e338a.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
